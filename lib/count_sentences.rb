a468ffbf25adb801432c74d3600cd16f55fa622c
Xavier Perez <xperez007@gmail.com>
2020-02-10 15:36:43 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,29 +1,6 @@
 require 'pry'
 
 class String
-  
-  def count_sentences?
-    
-  end
-  
-  def question?
-    
-  end
-  
-  def exclamation?
-    
-  end
-  
-  def count_sentences?
-    
-  end
-  
-  
-  
-
-require 'pry'
-
-class String
 
   def count_sentences?
     
@@ -41,3 +18,19 @@ class String
 
   end
 end
+
+require 'pry'
+
+class String
+  
+  def sentence? 
+  end
+  
+  "HI!".end_with?("!")
+  => true 
+  end
+  
+  def question?
+end
+
+  
